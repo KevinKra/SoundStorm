8039c173e31fc026678d404e4925c5f629c7a429
Kevin Krato <kratok08@gmail.com>
2019-07-24 17:38:51 -0600

    Mock out MainPage

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -11,13 +11,35 @@ export default class MainPage extends Component {
             <nav>ROCK HIPHOP POP</nav>
           </header>
           <section className="CardBoard">
-            <article className="Card">
-              <button>Click</button>
-            </article>
-            <article className="Card">CARD</article>
-            <article className="Card">CARD</article>
-            <article className="Card">CARD</article>
-            <article className="Card">CARD</article>
+            <div className="TrackList">
+              <article className="Card">
+                <button>Click</button>
+              </article>
+              <article className="Card">CARD</article>
+              <article className="Card">CARD</article>
+              <article className="Card">CARD</article>
+              <article className="Card">CARD</article>
+              <article className="Card">CARD</article>
+            </div>
+            <section className="TrackDetails">TrackDetails</section>
+          </section>
+          <section className="SupportContent">
+            <h1>Spotlight</h1>
+            <div className="Genres">
+              <article className="Genre">GENRE 1</article>
+              <article className="Genre">GENRE 2</article>
+              <article className="Genre">GENRE 3</article>
+              <article className="Genre">GENRE 4</article>
+            </div>
+          </section>
+          <section className="SupportContent">
+            <h1>Spotlight 2</h1>
+            <div className="Genres">
+              <article className="Genre">GENRE 1</article>
+              <article className="Genre">GENRE 2</article>
+              <article className="Genre">GENRE 3</article>
+              <article className="Genre">GENRE 4</article>
+            </div>
           </section>
           <section className="SupportPage">
             <section className="CardControls">
