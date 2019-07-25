1566e3cd4beb5275d7971bd845a6c9431c879e7c
Kevin Krato <kratok08@gmail.com>
2019-07-25 15:00:50 -0600

    fetch related songs and then filter out already selected song

--- src/_api/apiCalls.js
+++ src/_api/apiCalls.js
@@ -13,3 +13,17 @@ export const fetchGenres = async (genreType, count) => {
   });
   return parsed.results;
 };
+
+export const fetchRelatedSongs = async (genres, count, currentSongId) => {
+  const response = await fetch(
+    `https://api.jamendo.com/v3.0/tracks/?client_id=${API_KEY}&format=jsonpretty&limit=${count}&include=musicinfo&tags=${genres}`
+  );
+  const parsed = await response.json();
+  parsed.results.forEach(song => {
+    delete song.waveform;
+  });
+  const filteredResults = parsed.results.filter(result => {
+    return result.id !== currentSongId;
+  });
+  return filteredResults;
+};
