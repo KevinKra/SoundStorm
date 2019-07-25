84b705b2286fad54ceee06128af4568194737d84
Kevin Krato <kratok08@gmail.com>
2019-07-24 20:08:54 -0600

    Interact with track data

--- src/components/TrackCard/TrackCard.js
+++ src/components/TrackCard/TrackCard.js
@@ -1,6 +1,21 @@
 import React from "react";
 import "./TrackCard.scss";
 
-export default function Card() {
-  return <article className="TrackCard">Card</article>;
+export default function TrackCard(props) {
+  const {
+    album_image,
+    album_name,
+    name,
+    artist_idstr,
+    release_date,
+    audio
+  } = props.data;
+
+  return (
+    <article className="TrackCard">
+      <img src={album_image} alt={album_name} />
+      {/* <h4>{name}</h4> */}
+      <audio controls src={audio} />
+    </article>
+  );
 }
