73f6cff90f14d85c8b515e2a71e451a01dc23d07
Kevin Krato <kratok08@gmail.com>
2019-07-24 19:15:12 -0600

    Clean fetch genres data

--- src/_api/apiCalls.js
+++ src/_api/apiCalls.js
@@ -5,5 +5,9 @@ export const fetchGenres = async (genreType, count) => {
     `https://api.jamendo.com/v3.0/tracks/?client_id=${API_KEY}&format=jsonpretty&limit=${count}&fuzzytags=${genreType}&include=musicinfo&groupby=artist_id`
   );
   const parsed = await response.json();
+  //clean data
+  parsed.results.forEach(song => {
+    return delete song.waveform, delete song.license_ccurl, delete song.prourl;
+  });
   return parsed.results;
 };
