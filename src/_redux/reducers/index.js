2971b274805a759cf6de7b112b2fedcbe3d7e7f4
Kevin Krato <kratok08@gmail.com>
2019-07-25 15:02:58 -0600

    Update currentSong reducer

--- src/_redux/reducers/index.js
+++ src/_redux/reducers/index.js
@@ -7,10 +7,20 @@ export const primaryTracks = (state = [], action) => {
   }
 };
 
-export const currentPlaylist = (state = [], action) => {
+export const currentSong = (state = [], action) => {
   switch (action.type) {
-    case "LOAD_SOMETHING":
-      return action.payload;
+    case "LOAD_TARGET_SONG":
+      return action.song;
+    case "PLAY_SONG":
+      const playingSong = { ...action.song };
+      playingSong.playing = true;
+      return playingSong;
+    case "PAUSE_SONG":
+      const pausedSong = { ...action.song };
+      pausedSong.playing = false;
+      return pausedSong;
+    case "NEXT_SONG":
+      return state;
     default:
       return state;
   }
