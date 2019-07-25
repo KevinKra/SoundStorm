06ffc27d8cc577bec79838e46e40c8d42d85659e
Kevin Krato <kratok08@gmail.com>
2019-07-24 18:28:56 -0600

    Update component structuring

--- src/components/PrimaryTracks/PrimaryTracks.js
+++ src/components/PrimaryTracks/PrimaryTracks.js
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
 import * as apiCalls from "../../_api/apiCalls";
+import TrackCard from "../TrackCard/TrackCard";
 import "./PrimaryTracks.scss";
 
-export default class CardBoard extends Component {
+export default class PrimaryTracks extends Component {
   async componentDidMount() {
-    await apiCalls.fetchGenres("hiphop");
+    await apiCalls.fetchGenres("hiphop", 6);
   }
   render() {
     return (
       <section className="PrimaryTracks">
         <div className="TrackList">
-          <article className="Card">
-            <button>Click</button>
-          </article>
-          <article className="Card">CARD</article>
-          <article className="Card">CARD</article>
-          <article className="Card">CARD</article>
-          <article className="Card">CARD</article>
-          <article className="Card">CARD</article>
+          <TrackCard />
+          <TrackCard />
+          <TrackCard />
+          <TrackCard />
+          <TrackCard />
+          <TrackCard />
         </div>
         <section className="TrackDetails">TrackDetails</section>
       </section>
