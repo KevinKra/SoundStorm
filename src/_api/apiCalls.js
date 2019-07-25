21634346f27fdd594b2d412b087afc0f9aa3851a
Kevin Krato <kratok08@gmail.com>
2019-07-24 18:27:19 -0600

    Update call to include fetch count

--- src/_api/apiCalls.js
+++ src/_api/apiCalls.js
@@ -1,8 +1,8 @@
 import API_KEY from "./apiKey";
 
-export const fetchGenres = async genreType => {
+export const fetchGenres = async (genreType, count) => {
   const result = await fetch(
-    `https://api.jamendo.com/v3.0/tracks/?client_id=${API_KEY}&format=jsonpretty&limit=5&fuzzytags=${genreType}&include=musicinfo&groupby=artist_id`
+    `https://api.jamendo.com/v3.0/tracks/?client_id=${API_KEY}&format=jsonpretty&limit=${count}&fuzzytags=${genreType}&include=musicinfo&groupby=artist_id`
   );
   console.log(await result.json());
 };
