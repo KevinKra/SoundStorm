7e3acdc3ca89b99f4a7ed915cf1aeceaf70eef90
Kevin Krato <kratok08@gmail.com>
2019-07-25 16:45:39 -0600

    Restore playing property to currentSong reducer

--- src/_redux/reducers/index.js
+++ src/_redux/reducers/index.js
@@ -11,6 +11,14 @@ export const currentSong = (state = {}, action) => {
   switch (action.type) {
     case "PLAY_TARGET_SONG":
       return action.song;
+    case "PLAY_SONG":
+      const playingSong = { ...action.song };
+      playingSong.playing = true;
+      return playingSong;
+    case "PAUSE_SONG":
+      const pausedSong = { ...action.song };
+      pausedSong.playing = false;
+      return pausedSong;
     default:
       return state;
   }
