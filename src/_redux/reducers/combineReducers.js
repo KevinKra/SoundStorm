d69370c64a69b940f01b3c40264f42236c5438c5
Kevin Krato <kratok08@gmail.com>
2019-07-25 10:25:49 -0600

    Combine new reducer

--- src/_redux/reducers/combineReducers.js
+++ src/_redux/reducers/combineReducers.js
@@ -2,5 +2,6 @@ import * as reducers from "./index";
 import { combineReducers } from "redux";
 
 export const rootReducer = combineReducers({
-  primaryTracks: reducers.primaryTracks
+  primaryTracks: reducers.primaryTracks,
+  currentPlaylist: reducers.currentPlaylist
 });
