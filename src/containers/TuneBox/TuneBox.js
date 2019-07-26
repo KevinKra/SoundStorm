95c04c0cee122a8f90047737f2a5f9178ba10e3e
Kevin Krato <kratok08@gmail.com>
2019-07-26 17:43:31 -0600

    Build first pass TuneBox styles

--- src/containers/TuneBox/TuneBox.js
+++ src/containers/TuneBox/TuneBox.js
@@ -1,26 +1,65 @@
 import React, { Component } from "react";
+import Radium from "radium";
 import "./TuneBox.scss";
 
-export default class TuneBox extends Component {
+class TuneBox extends Component {
+  state = {
+    activeHover: false
+  };
+
+  handleMouseEnter = () => {
+    this.setState({ activeHover: true });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({ activeHover: false });
+  };
+
   render() {
-    const passedStyles = {
-      height: "300px",
-      width: "275px",
-      color: "white"
+    const styles = {
+      base: {
+        height: "300px",
+        width: `${this.props.width}`,
+        color: "white"
+      }
     };
-    const imageStyles = {
+    const topImage = {
       position: "relative",
-      // zIndex: "1",
-      backgroundImage: `url(${this.props.image})`,
+      zIndex: "2",
+      backgroundImage: `url(${this.props.topImage})`,
       backgroundSize: "cover",
+      backgroundPosition: "center",
       height: "80%",
-      width: "100%"
+      width: "100%",
+      transition: "0.5s transform ease-in-out",
+      transform: `translateY(${this.state.activeHover ? "-100%" : "0%"})`
+    };
+    const bottomImage = {
+      position: "absolute",
+      zIndex: "1",
+      backgroundImage: `url(${this.props.bottomImage})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      height: "80%",
+      width: "100%",
+      transition: "0.5s transform ease-in-out"
+      // transform: `translateY(${this.state.activeHover ? "-100%" : "0%"})`
     };
 
     return (
-      <section className="TuneBox" style={passedStyles}>
-        <div className="image" style={imageStyles}>
-          <h2 className="card-title">{`${this.props.title}`}</h2>
+      <section
+        className="TuneBox"
+        style={[styles.base]}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+      >
+        <div className="image" style={topImage}>
+          <h2 className="card-title">{`${this.props.primaryTitle}`}</h2>
+        </div>
+        <div className="image" style={bottomImage}>
+          <p className="card-title playlist-title">{`${
+            this.props.playlistTitle
+          }`}</p>
         </div>
         <section className="card-body">
           <div>
@@ -33,3 +72,5 @@ export default class TuneBox extends Component {
     );
   }
 }
+
+export default Radium(TuneBox);
