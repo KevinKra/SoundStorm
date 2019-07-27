2f25f4716656d963cab445d34621c52f37bf2ce2
Kevin Krato <kratok08@gmail.com>
2019-07-27 12:42:19 -0600

    Add HeroImage component and include prop

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -4,12 +4,18 @@ import { PrimaryTracks } from "../PrimaryTracks/PrimaryTracks";
 import SupportContent from "../SupportContent/SupportContent";
 import SupportPage from "../SupportPage/SupportPage";
 import Header from "../Header/Header";
+import HeroImage from "../HeroImage/HeroImage";
 
 export default class MainPage extends Component {
   render() {
     return (
       <section className="MainPage">
         <Header />
+        <HeroImage
+          image={
+            "https://images.unsplash.com/photo-1501612780327-45045538702b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+          }
+        />
         <PrimaryTracks />
         <SupportContent />
         <SupportPage />
