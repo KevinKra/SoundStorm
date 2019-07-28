13e0620848e0513e3ff409000f19cff6b2cfbf59
Kevin Krato <kratok08@gmail.com>
2019-07-28 13:36:06 -0600

    write snapshot tests for Header, HeroImage, MainPage components

--- src/components/MainPage/MainPage.test.js
+++ src/components/MainPage/MainPage.test.js
@@ -1,10 +1,11 @@
 import React from "react";
-import App from "./App";
+import MainPage from "./MainPage";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 
-describe("<App />", () => {
-  let wrapper = shallow(<App />);
+describe("<MainPage />", () => {
+  let wrapper = shallow(<MainPage />);
+  console.log(wrapper.debug());
   it("should match the snapshot", () => {
     expect(toJson(wrapper)).toMatchSnapshot();
   });
