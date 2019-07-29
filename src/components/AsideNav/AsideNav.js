6068ef111c0d34fdb5131fab71d8ea8480bc99db
Kevin Krato <kratok08@gmail.com>
2019-07-29 11:26:13 -0600

    Integrate redux and navlink router interaction

--- src/components/AsideNav/AsideNav.js
+++ src/components/AsideNav/AsideNav.js
@@ -1,16 +1,44 @@
 import React from "react";
 import "./AsideNav.scss";
+import { NavLink } from "react-router-dom";
+import * as actions from "../../_redux/actions/";
+import { connect } from "react-redux";
 
-export default function AsideNav() {
+function AsideNav(props) {
   return (
     <aside className="AsideNav">
+      <section className="logo-section">
+        <div className="logo-background">
+          <i class="fas fa-3x fa-music company-logo" />
+        </div>
+      </section>
       <nav>
-        <p>Aside Nav</p>
-        <p>ITEM</p>
-        <p>ITEM</p>
-        <p>ITEM</p>
-        <p>ITEM</p>
+        <NavLink className="aside-link" activeClassName="active" to="/home">
+          Home
+        </NavLink>
+        <NavLink
+          className="aside-link"
+          activeClassName="active"
+          to="/browse"
+          onClick={() => props.clearSong()}
+        >
+          Browse
+        </NavLink>
+        <NavLink className="aside-link" activeClassName="active" to="/artist">
+          Artists
+        </NavLink>
       </nav>
+      <div className="placeholder" />
     </aside>
   );
 }
+
+const mapDispatchToProps = dispatch => ({
+  resetIndex: () => dispatch(actions.resetIndex()),
+  clearSong: () => dispatch(actions.clearSong())
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(AsideNav);
