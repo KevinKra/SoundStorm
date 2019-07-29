c17119cd5ae31454730cd504cb8f7b9d59585807
Kevin Krato <kratok08@gmail.com>
2019-07-28 18:41:27 -0600

    Import mock data

--- src/_api/apiCalls.test.js
+++ src/_api/apiCalls.test.js
@@ -1,52 +1,6 @@
 import { fetchGenres, fetchRelatedSongs } from "./apiCalls";
+import { mockFetchData } from "../mockData/index";
 import API_KEY from "./apiKey";
-
-const mockFetchData = {
-  results: [
-    {
-      id: "1197872",
-      name: "Perverting The American Dream",
-      duration: 238,
-      artist_id: "462184",
-      artist_name: "The Bourgeois",
-      artist_idstr: "The_Bourgeois",
-      album_name: "The Bourgeois",
-      album_id: "144113",
-      position: 1,
-      releasedate: "2015-02-16",
-      album_image:
-        "https://imgjam2.jamendo.com/albums/s144/144113/covers/1.200.jpg",
-      audio:
-        "https://mp3l.jamendo.com/?trackid=1197872&format=mp31&from=app-d011137e",
-      audiodownload: "https://mp3d.jamendo.com/download/track/1197872/mp32/",
-      shorturl: "http://jamen.do/t/1197872",
-      shareurl: "http://www.jamendo.com/track/1197872",
-      image: "https://imgjam2.jamendo.com/albums/s144/144113/covers/1.200.jpg",
-      playing: true
-    },
-    {
-      id: "1231231",
-      name: "AnotherSong",
-      duration: 222,
-      artist_id: "452184",
-      artist_name: "The Other song",
-      artist_idstr: "The_Other_song",
-      album_name: "The Other song",
-      album_id: "154113",
-      position: 1,
-      releasedate: "2012-02-16",
-      album_image:
-        "https://imgjam2.jamendo.com/albums/s144/144113/covers/1.200.jpg",
-      audio:
-        "https://mp3l.jamendo.com/?trackid=1197872&format=mp31&from=app-d011137e",
-      audiodownload: "https://mp3d.jamendo.com/download/track/1197872/mp32/",
-      shorturl: "http://jamen.do/t/1197872",
-      shareurl: "http://www.jamendo.com/track/1197872",
-      image: "https://imgjam2.jamendo.com/albums/s144/144113/covers/1.200.jpg",
-      playing: false
-    }
-  ]
-};
 
 describe("fetchGenres", () => {
   beforeEach(() => {
