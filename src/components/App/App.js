d9707c8abc5d463c144337df69fbabb2250466d6
Kevin Krato <kratok08@gmail.com>
2019-07-30 08:08:43 -0600

    Update route pathing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,18 @@ import React, { Component, Fragment } from "react";
 import { Route } from "react-router-dom";
 import AsideNav from "../../containers/AsideNav/AsideNav";
 import "./App.css";
+import HomePage from "../HomePage/HomePage";
 import MainPage from "../MainPage/MainPage";
+import ArtistPage from "../ArtistPage/ArtistPage";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Route path="/" component={AsideNav} />
+        <Route exact path="/" component={HomePage} />
         <Route path="/browse" component={MainPage} />
+        <Route path="/artist" component={ArtistPage} />
       </Fragment>
     );
   }
