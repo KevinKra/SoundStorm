24bf93d308540bee8d78656a33140b28b8470dba
Kevin Krato <kratok08@gmail.com>
2019-07-30 08:32:04 -0600

    Update page under construction sign

--- src/components/ArtistPage/ArtistPage.js
+++ src/components/ArtistPage/ArtistPage.js
@@ -3,7 +3,10 @@ import "./ArtistPage.scss";
 export default function ArtistPage() {
   return (
     <main className="ArtistPage">
-      <p>Under construction</p>
+      <img
+        src="https://cdn.pixabay.com/photo/2017/06/16/07/26/under-construction-2408059_960_720.png"
+        alt="under construction"
+      />
     </main>
   );
 }
